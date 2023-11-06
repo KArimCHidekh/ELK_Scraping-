f0213ffe70bf7cdbc57af38bca2667b616674b17
KArimCHidekh <132760842+KArimCHidekh@users.noreply.github.com>
2023-11-06 14:29:40 +0100

    Update run_docker.sh

--- run_docker.sh
+++ run_docker.sh
@@ -1,1 +1,7 @@
+#!/bin/bash
 
+# Run the FastAPI app container, mapping port 8000 on the host to port 8000 in the container
+docker run -p 8000:8000 fastapi-app
+
+docker pull your-dockerhub-username/fastapi-app:latest
+docker run -d -p 8000:80 your-dockerhub-username/fastapi-app:latest
